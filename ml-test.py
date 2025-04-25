aac16a0ac155b8d615fbe35f090019f62caadd18
Javier Chacnama <jchacnama.q@gmail.com>
2025-04-25 16:03:09 -0500

    Creacion de la app web con el modelo de ml

--- ml-test.py
+++ ml-test.py
@@ -7,16 +7,24 @@ model = joblib.load('./model/insurance.pkl')
 sc_x = joblib.load('./model/scaler_x.pkl')
 sc_y = joblib.load('./model/scaler_y.pkl')
 
-print(f'sc_x: {sc_x.mean_}')
-print(f'sc_y: {sc_y.mean_}')
+# print(f'sc_x: {sc_x.mean_}')
+# print(f'sc_y: {sc_y.mean_}')
 
-# rooms = int(input('Ingrese nro de habitaciones: '))
-# rooms_sc = sc_x.transform(np.array([[rooms]]))
-# #print(f'rooms_sc: {rooms_sc}')
+edad = int(input('Ingrese la edad: '))
+edad_sc = sc_x.transform(np.array([edad]).reshape(-1,1))
+prediction = model.predict(edad_sc)
+prediction_sc = sc_y.inverse_transform(prediction)
+print(f"LOS GASTOS MEDICOS PARA UNA PERSONA CON {edad} años son {prediction_sc}")
+#print(f"LOS GASTOS MEDICOS PARA UNA PERSONA CON {edad} años son {sc_y.inverse_transform(model.predict(edad_sc))}")
 
-# prediction = model.predict(rooms_sc)
 
-# #print(f'prediction: {prediction}')
+
+# edad_sc = sc_x.transform(np.array([[edad]]))
+# #print(f'edad_sc: {edad_sc}')
+
+# prediction = model.predict(edad_sc)
+
+# # #print(f'prediction: {prediction}')
 
 # prediction_sc = sc_y.inverse_transform(prediction) * 1000
-# print(f'El precio de un departamento con {rooms} habitaciones es: $ {prediction_sc[0][0]:.2f}')
+# print(f'Los gastos medicos para una persona de {edad} años es: $ {prediction_sc[0][0]:.2f}')
